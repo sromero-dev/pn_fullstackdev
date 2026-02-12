43e2f98772a6bac47b6087b400874757667002c1
sromero-dev <sromerodiaz.work@gmail.com>
2026-02-12 12:23:45 +0100

    axios y crud

---
+++ gestion_tareas/frontend/src/services/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  // Familias CRUD
+  getFamilias() {
+    return apiClient.get("/familias/");
+  },
+  createFamilia(data) {
+    return apiClient.post("/familias/", data);
+  },
+  updateFamilia(id, data) {
+    return apiClient.put(`/familias/${id}/`, data);
+  },
+  deleteFamilia(id) {
+    return apiClient.delete(`/familias/${id}/`);
+  },
+
+  // Tareas CRUD
+  getTareas(params = {}) {
+    return apiClient.get("/tareas/", { params });
+  },
+  getTarea(id) {
+    return apiClient.get(`/tareas/${id}/`);
+  },
+  createTarea(data) {
+    return apiClient.post("/tareas/", data);
+  },
+  updateTarea(id, data) {
+    return apiClient.put(`/tareas/${id}/`, data);
+  },
+  deleteTarea(id) {
+    return apiClient.delete(`/tareas/${id}/`);
+  },
+};
