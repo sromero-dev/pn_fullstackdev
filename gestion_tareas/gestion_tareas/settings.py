223b57f40eae15c424364c64ceadcdffdaf9b576
sromero-dev <sromerodiaz.work@gmail.com>
2026-02-12 07:49:08 +0100

    framework instalado

--- gestion_tareas/gestion_tareas/settings.py
+++ gestion_tareas/gestion_tareas/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
     'tareas',
 ]
 
@@ -116,3 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Configuración del framework. También opcional pero recomendado según la documentación oficial.
+# Esta configuración habilita filtros, búsqueda, ordenamiento y paginación global.
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
