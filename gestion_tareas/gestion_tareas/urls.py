d9baff75ddae397db4ae9fcd28f66ae349c30bc1
sromero-dev <sromerodiaz.work@gmail.com>
2026-02-12 09:51:28 +0100

    urls configuradas

--- gestion_tareas/gestion_tareas/urls.py
+++ gestion_tareas/gestion_tareas/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tareas.views import TareaViewSet
+
+router = DefaultRouter()
+router.register('tareas', TareaViewSet, basename='tareas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), # Todas las rutas de la API
+    path('api-auth/', include('rest_framework.urls')) # Rutas de autenticación
 ]
