2ffb4a619986761b0fa2ecb29e23a51d20bbb6f7
sromero-dev <sromerodiaz.work@gmail.com>
2026-02-12 10:34:05 +0100

    validaciones

--- gestion_tareas/tareas/serializers.py
+++ gestion_tareas/tareas/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Tarea
+from django.utils import timezone
 
 class TareaSerializer(serializers.ModelSerializer):
   """Serializer para el modelo Tarea."""
@@ -15,3 +16,16 @@ class TareaSerializer(serializers.ModelSerializer):
       'fecha_creacion', 'fecha_vencimiento'
     ]
     read_only_fields = ['fecha_creacion'] # Define campos que no se pueden modificar
+
+  # ? DRF busca automáticamente métodos que empiecen con la palabra validate_ seguidos del nombre de un campo para aplicar reglas de negocio
+  def validate_fecha_vencimiento(self, value):
+        """Evita que un usuario cree una tarea que ya expiró"""
+        if value and value < timezone.now().date():
+            raise serializers.ValidationError("La fecha de vencimiento no puede ser anterior a hoy.")
+        return value
+
+  def validate_estado(self, value):
+      """Convierte las opciones del modelo en un diccionario para verificar rápidamente si el estado enviado existe."""
+      if value not in dict(Tarea.Estado.choices): # Lo hace 'choices' por defecto pero es una capa de seguridad extra
+          raise serializers.ValidationError("Estado no válido.")
+      return value
