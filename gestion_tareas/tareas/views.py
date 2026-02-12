76d3d9fbf65931ea9803d7024d069c940690a267
sromero-dev <sromerodiaz.work@gmail.com>
2026-02-12 09:47:07 +0100

    viewset

--- gestion_tareas/tareas/views.py
+++ gestion_tareas/tareas/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import Tarea
+from .serializers import TareaSerializer
+from .filters import TareaFilter
 
 # Create your views here.
+class TareaViewSet(viewsets.ModelViewSet):
+  """
+  ViewSet para CRUD de Tareas.
+  Django rest framework genera automáticamente el CRUD al heredar de ModelViewSet.
+  Permite filtrar, buscar y ordenar las tareas.
+  """
+  queryset = Tarea.objects.select_related('familia').all() # Indica de donde se van a obtener los datos. Emplear `select_related` trae la información de las familias relacionadas a dicha tarea, dicho de otra manera, es como un JOIN de SQL
+  serializer_class = TareaSerializer # Especifica el serializador que convierta los objetos en JSON
+  filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter] # Indica los filtros que se van a usar: DjangoFilterBackend (filtro por campos exactos), SearchFilter (barra de búsqueda) y OrderingFilter (permite elegir el orden de resultado de las tareas)
+  filterset_class = TareaFilter # uso de'.filters.py', ver bibliografia sobre filtros personalizados.
+  search_fields = ['titulo', 'descripcion'] # Indica los campos por los cuales se puede buscar '?search=...'
+  ordering_fields = ['fecha_creacion', 'fecha_vencimiento', 'estado'] # Establece los campos por los cuales se puede ordenar la lista '?ordering=...'
